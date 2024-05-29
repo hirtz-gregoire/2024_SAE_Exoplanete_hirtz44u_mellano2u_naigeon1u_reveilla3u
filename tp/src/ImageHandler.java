08df73ce6a9d12a891db80f66f33e2f3ae372ffe
GregoireHirtz <hirtz.gregoire@ghmail.fr>
2024-05-29 09:16:39 +0200

    initialisation + q1.1, 1.2, 1.3 + debut 1.4

---
+++ tp/src/ImageHandler.java
@@ -0,0 +1,69 @@
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import javax.imageio.*;
+
+public class ImageHandler {
+
+    public static void main(String[] args) {
+
+        try{
+            new ImageHandler().grayscaleDuplicateImage("tp/ressource/src/img1.png", "tp/ressource/out/img2.png");
+        }catch (IOException e){
+            System.out.println(e);
+        }
+
+    }
+
+
+    public void duplicateImage(String in, String out) throws IOException {
+        // Load the input picture
+        BufferedImage bufferedImage = ImageIO.read(new File(in));
+
+        // copy input to output
+        ImageIO.write(bufferedImage, "png", new File(out));
+    }
+
+
+    public void duplicateImageByPixel(String in, String out) throws IOException {
+        // Load the input picture and create the vessel
+        BufferedImage image = ImageIO.read(new File(in));
+        BufferedImage vessel = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+
+        // Copy the image into the vessel and save the result
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                vessel.setRGB(x, y, image.getRGB(x, y));
+            }
+        }
+
+        // save picture
+        ImageIO.write(vessel, "png", new File(out));
+    }
+
+    public void grayscaleDuplicateImage(String in, String out) throws IOException {
+        // Load the input picture and create the vessel
+        BufferedImage image = ImageIO.read(new File(in));
+        BufferedImage vessel = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+
+        // Copy the image into the vessel and save the result
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                int[] color = ColorTool.getTabColor(image.getRGB(x, y));
+                //0.299 ∙ Red + 0.587 ∙ Green + 0.114 ∙ Blue
+                //int greyScale = (int)(color[2] * 0.114 + color[1] * 0.587 + color[0] * 0.299);
+                int greyScale = (color[0]+color[1]+color[2])/3;
+                //System.out.println(Integer.toBinaryString(greyScale));
+                vessel.setRGB(x, y, greyScale);
+            }
+        }
+
+        // save picture
+        ImageIO.write(vessel, "png", new File(out));
+    }
+}
