fd5e0367b1e5950d41e9c2fdd9114faf69b6dd93
Fiera <46507472+Fiera42@users.noreply.github.com>
2024-06-18 15:28:02 +0200

    rename variable

--- sae/src/tools/ColorTool.java
+++ sae/src/tools/ColorTool.java
@@ -2,12 +2,12 @@ package tools;
 
 public class ColorTool {
 
-    public static int[] getTabColor(int c) {
+    public static int[] getTabColor(int colorInteger) {
         int[] res = new int[3];
 
-        res[0] = c & 0xff ; // Blue
-        res[1] = ( c & 0xff00 ) >> 8 ; // Green
-        res[2] = ( c & 0xff0000) >> 16; // Red
+        res[0] = colorInteger & 0xff ; // Blue
+        res[1] = ( colorInteger & 0xff00 ) >> 8 ; // Green
+        res[2] = ( colorInteger & 0xff0000) >> 16; // Red
 
         return res;
     }
