12c0b0f32c380313c28b9588553dd1265204703f
Fiera <46507472+Fiera42@users.noreply.github.com>
2024-06-18 17:14:21 +0200

    entisity filter

---
+++ sae/src/image_processors/processors/ItensityFilter.java
@@ -0,0 +1,36 @@
+package image_processors.processors;
+
+import image_processors.Processor;
+import tools.ColorTool;
+
+import java.awt.image.BufferedImage;
+
+import static java.lang.Math.round;
+
+public class ItensityFilter implements Processor {
+
+    private double intensity;
+    public ItensityFilter() {
+        this.intensity = 75;
+    }
+
+    public ItensityFilter(double intensity) {
+        this.intensity = intensity;
+    }
+    @Override
+    public BufferedImage process(BufferedImage image) {
+
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                int[] rbgValues = ColorTool.getTabColor(image.getRGB(x, y));
+                rbgValues[0] = (int)round(rbgValues[0] + intensity/100 * (255 - rbgValues[0]));
+                rbgValues[1] = (int)round(rbgValues[1] + intensity/100 * (255 - rbgValues[1]));
+                rbgValues[2] = (int)round(rbgValues[2] + intensity/100 * (255 - rbgValues[2]));
+
+                image.setRGB(x, y, image.getRGB(x, y));
+            }
+        }
+
+        return image;
+    }
+}
