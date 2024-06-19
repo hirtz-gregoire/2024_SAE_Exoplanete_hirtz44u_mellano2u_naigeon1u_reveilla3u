96e137cc1fe91694291827b3abf963dfcd8a35d3
Fiera <46507472+Fiera42@users.noreply.github.com>
2024-06-19 12:50:46 +0200

    better imageDisplay

--- sae/src/image_processors/processors/Clusterer.java
+++ sae/src/image_processors/processors/Clusterer.java
@@ -1,9 +1,11 @@
 package image_processors.processors;
 
+import image_processors.ImageProcessor;
 import image_processors.Processor;
 import tools.cluster.Clustering;
 import tools.ColorTool;
 import tools.Palette;
+import tools.cluster.KMeans;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -25,6 +27,26 @@ public class Clusterer implements Processor {
     private int clusterMode;
     private StepExporter exporter;
 
+    public Clusterer(Clustering clusteringAlgorithm, int clusterMode) {
+        this.clusteringAlgorithm = clusteringAlgorithm;
+        this.palette = null;
+        if(clusterMode < 0 || clusterMode > 2) {
+            clusterMode = 0;
+        }
+        this.clusterMode = clusterMode;
+        this.exporter = null;
+    }
+
+    public Clusterer(Clustering clusteringAlgorithm, int clusterMode, StepExporter exporter) {
+        this.clusteringAlgorithm = clusteringAlgorithm;
+        this.palette = null;
+        if(clusterMode < 0 || clusterMode > 2) {
+            clusterMode = 0;
+        }
+        this.clusterMode = clusterMode;
+        this.exporter = exporter;
+    }
+
      public Clusterer(Clustering clusteringAlgorithm, int clusterMode, StepExporter exporter, Palette palette) {
          this.clusteringAlgorithm = clusteringAlgorithm;
          this.palette = palette;
@@ -36,10 +58,10 @@ public class Clusterer implements Processor {
      }
     @Override
     public BufferedImage process(BufferedImage image) {
-         System.out.println("Clusterer processing...");
+        System.out.println("Clusterer processing...");
         ArrayList<double[]> data = new ArrayList<>();
 
-        // foreach pixel
+        // Generate the points to cluster
         for (int x = 0; x < image.getWidth(); x++) {
             for (int y = 0; y < image.getHeight(); y++) {
                 double[] pixelData = new double[0];
@@ -66,9 +88,16 @@ public class Clusterer implements Processor {
 
         System.out.println("Clustering values...");
         int[] clusters = clusteringAlgorithm.cluster(data.toArray(new double[0][0]));
+
+        // Output additional images when clustering by color
+        if(exporter != null) {
+            System.out.println("Generating clustered output images");
+            drawStepImages(image, clusters);
+        }
+
+        // Output image is all clusters
         int maxValue = Arrays.stream(clusters).max().getAsInt();
         int i = 0;
-
         for (int x = 0; x < image.getWidth(); x++) {
             for (int y = 0; y < image.getHeight(); y++) {
                 image.setRGB(x, y, getColorGradient(clusters[i++], maxValue).getRGB());
@@ -84,5 +113,101 @@ public class Clusterer implements Processor {
 
         return Color.getHSBColor(hue, 1.0f, 1.0f);
     }
+
+    public void drawStepImages(BufferedImage image, int[] clusters) {
+        // Image processor to create background images
+        Processor[] processes = {
+                new DuplicateImageByPixel(),
+                new IntensityFilter(),
+        };
+
+        ImageProcessor imageProcessor = new ImageProcessor(processes);
+
+        // When we have a palette, we draw clusters using it
+        if(palette != null) {
+            switch (clusterMode) {
+                case CLUSTER_BY_COLOR:
+                    exportImageWithPaletteCLUSTER_COLOR(image, imageProcessor);
+                    break;
+                case CLUSTER_BY_COLOR_AND_POSITION:
+                    exportImageWithPaletteCLUSTER_COLOR_POSITION(image, imageProcessor, clusters);
+                    break;
+            }
+        }
+        // If we don't have palette, we draw individual clusters
+        else {
+            switch (clusterMode) {
+                case CLUSTER_BY_COLOR:
+                case CLUSTER_BY_COLOR_AND_POSITION:
+                    exportClusteredImage(image, imageProcessor, clusters);
+                    break;
+            }
+        }
+    }
+
+    private void exportClusteredImage(BufferedImage image, ImageProcessor processor, int[] clusters) {
+        int maxValue = Arrays.stream(clusters).max().getAsInt();
+
+        for(int i = 0; i < maxValue; i++) {
+            // Create the background
+            BufferedImage outputImage = processor.processImage(image);
+
+            // Draw the colors on it
+            int k = 0;
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    if(clusters[k++] == i) {
+                        outputImage.setRGB(x, y, getColorGradient(clusters[k], maxValue).getRGB());
+                    }
+                }
+            }
+
+            // Output the image
+            exporter.process(outputImage);
+        }
+    }
+
+    private void exportImageWithPaletteCLUSTER_COLOR(BufferedImage image, ImageProcessor processor) {
+        // When a palette is given, we assume it's the palette of the image and just draw the pixels that match the palette
+        for(Color color : palette.getColors()) {
+            // Create the background
+            BufferedImage outputImage = processor.processImage(image);
+
+            // Draw the colors on it
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    if(image.getRGB(x, y) == color.getRGB()) {
+                        outputImage.setRGB(x, y, color.getRGB());
+                    }
+                }
+            }
+
+            // Output the image
+            exporter.process(outputImage);
+        }
+    }
+
+    private void exportImageWithPaletteCLUSTER_COLOR_POSITION(BufferedImage image, ImageProcessor processor, int[] clusters) {
+        // When a palette is given, we assume it's the palette of the image and just draw the pixels that match the palette
+        for(Color color : palette.getColors()) {
+            // Create the background
+            BufferedImage outputImage = processor.processImage(image);
+
+            // Draw the colors on it
+            int maxValue = Arrays.stream(clusters).max().getAsInt();
+            int i = 0;
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    if(image.getRGB(x, y) == color.getRGB()) {
+                        outputImage.setRGB(x, y, getColorGradient(clusters[i++], maxValue).getRGB());
+                    }
+                }
+            }
+
+            // Output the image
+            exporter.process(outputImage);
+        }
+    }
+
 
 }
